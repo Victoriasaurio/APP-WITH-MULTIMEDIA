cf02e2cc5fce24f44eba16322ec166e0639bae06
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-12-04 13:57:42 -0600

    Fourth commit

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,19 +2,37 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imagesvg;
+
+    private ObjectAnimator animatorRotation;
+
+    private long animationDuration = 7000;
+
+    private AnimatorSet animatorSet;
+
     MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        animatorSet = new AnimatorSet();
+
+        imagesvg = findViewById(R.id.imagesvg);
     }
 
     public void play(View v) {
@@ -27,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-
+        animacion();
         player.start();
     }
 
@@ -58,5 +76,21 @@ public class MainActivity extends AppCompatActivity {
     public void video(View view) {
         Intent intent = new Intent(this, video.class);
         startActivity(intent);
+    }
+
+    public void animacion() {
+        animatorRotation = ObjectAnimator.ofFloat(imagesvg, "rotation",0f, 360f);
+        animatorRotation.setDuration(animationDuration);
+        AnimatorSet animatorSetRotator = new AnimatorSet();
+        animatorSetRotator.playTogether(animatorRotation);
+        //animatorSetRotator.start();
+        animatorSetRotator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //super.onAnimationEnd(animation);
+                animation.start();
+            }
+        });
+        animatorSetRotator.start();
     }
 }
